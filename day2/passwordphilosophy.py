edd07ceec4c6d906208fe7e2797e4a9f16b6e5ca
kyleman <kyleborah1234321@gmail.com>
2020-12-02 15:56:02 -0600

    day2 part2 answer

--- day2/passwordphilosophy.py
+++ day2/passwordphilosophy.py
@@ -13,4 +13,19 @@ for pw in password_list:
   print(pw)
   correct_password_count += 1
 
-print("{0} valid passwords".format(correct_password_count))
+print("{0} valid sled shop passwords.".format(correct_password_count))
+
+correct_password_count = 0
+
+for pw in password_list:
+ scheme = pw.split()
+ first = int(scheme[0].split("-")[0]) - 1
+ second = int(scheme[0].split("-")[1]) - 1
+ char = scheme[1].strip(":")
+ pwd = scheme[2]
+ if pwd[first] != pwd[second]:
+  if pwd[first] == char or pwd[second] == char:
+   print(pw)
+   correct_password_count += 1
+
+print("{0} valid toboggan shop passwords.".format(correct_password_count))
