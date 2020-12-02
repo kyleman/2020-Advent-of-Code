4244449bef5cdbf203bc26c4484482390ff68c75
kyleman <kyleborah1234321@gmail.com>
2020-12-02 10:09:44 -0600

    part2 initial solution
    This get dups. Need to figure out how to not get same answers because of the commutative property.

--- day1/reportrepair.py
+++ day1/reportrepair.py
@@ -1,6 +1,8 @@
 with open("report.txt", "r") as report:
  report_data = [int(number.strip()) for number in report] # need int() or list is a list of strings
 
+print("part1")
+
 l = len(report_data)
 i = 0
 
@@ -15,6 +17,16 @@ for i in range(0, l):
    print("{0} + {1} = 2020".format(report_data[i], report_data[j]))
    print("{0} * {1} = {2}".format(report_data[i], report_data[j], report_data[i] * report_data[j]))
 
-# answer
+# part1 answer
 # 976 + 1044 = 2020
 # 976 * 1044 = 1018944
+
+print("part2")
+
+#do this the inefficient way
+for i in report_data:
+ for j in report_data:
+  for k in report_data:
+   if i + j + k == 2020:
+    print("{0} + {1} + {2} = 2020".format(i, j, k))
+    print("{0} * {1} * {2} = {3}".format(i, j, k, i * j * k))
