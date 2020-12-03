03013d992d43560de9ae7a01745934af4cfa61b3
Kyle Borah <kyleborah1234321@gmail.com>
2020-12-03 12:00:23 -0600

    a working solution but algorithm is wrong

--- day3/toboggantrajectory.py
+++ day3/toboggantrajectory.py
@@ -3,3 +3,14 @@ runs = list()
 with open("mountain.txt", "r") as mountain:
  runs = [[r for r in run.strip()] for run in mountain]
 
+num_trees = 0
+x = 0 #position on x axis
+
+for run in runs:
+ l = len(run)
+ if run[x] == '#':
+  num_trees += 1
+ else:
+  x = (x + 3) % l #loop around if we go off the end of the list because the same pattern repeats
+
+print(num_trees)
