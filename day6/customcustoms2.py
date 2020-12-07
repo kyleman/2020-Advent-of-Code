944ec9c38a12be7f0ec32fe5eb2af34200f33af2
Kyle Borah <kyleborah1234321@gmail.com>
2020-12-06 18:15:59 -0600

    day6 part2 answer

---
+++ day6/customcustoms2.py
@@ -0,0 +1,16 @@
+def getIntersection(group: [set]) -> set:
+ intersection = group[0]
+ for g in group:
+  intersection = intersection.intersection(g)
+ return intersection
+
+with open("questions.txt", "r") as questions:
+ answers = [[{c for c in a} for a in q.split()] for q in questions.read().split("\n\n")]
+
+master_answers = []
+
+for group in answers:
+ group_answers = getIntersection(group)
+ master_answers.append(group_answers)
+
+print("{0} total answers without dups.".format(sum([len(master_answers[i]) for i in range(0, len(master_answers))])))
